bd1e4091048053cd164534df2e3ae2cd1c560f9a
Hernan Lozano <hernantz@gmail.com>
2019-12-23 19:44:18 -0300

    Moved config to a static file

--- setup.py
+++ setup.py
@@ -1,30 +1,3 @@
 from distutils.core import setup # setuptools breaks
 
-# Dynamically calculate the version based on knowledge.VERSION
-version = __import__('knowledge').__version__
-
-setup(
-    name = 'django-knowledge',
-    description = '''A simple frontend and admin interface for dealing with help
-        knowledge tickets and issues, including public and private responses and searching.''',
-    version = version,
-    author = 'Bryan Helmig',
-    author_email = 'bryan@zapier.com',
-    url = 'http://github.com/zapier/django-knowledge',
-    install_requires=['Markdown>=2.1.1','Django>=1.10'],
-    packages=['knowledge'],
-    package_data={'knowledge': [
-      'migrations/*.py',
-      'static/knowledge/css/*',
-      'templates/django_knowledge/*.html',
-      'templates/django_knowledge/emails/*.html',
-      'templatetags/*.py']},
-    classifiers = ['Development Status :: 3 - Alpha',
-                   'Environment :: Web Environment',
-                   'Framework :: Django',
-                   'Intended Audience :: Developers',
-                   'License :: OSI Approved :: BSD License',
-                   'Operating System :: OS Independent',
-                   'Programming Language :: Python',
-                   'Topic :: Utilities'],
-)
+setup()
